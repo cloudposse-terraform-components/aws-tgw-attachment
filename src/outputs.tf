59b0f687a8c606da74e104b447698df3c2d59936
milldr <miller0daniel@gmail.com>
2025-01-31 10:41:44 -0500

    upstream

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,4 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "transit_gateway_vpc_attachment_id" {
+  value       = module.standard_vpc_attachment.transit_gateway_vpc_attachment_ids["this"]
+  description = "ID of the Transit Gateway VPC Attachment"
 }
