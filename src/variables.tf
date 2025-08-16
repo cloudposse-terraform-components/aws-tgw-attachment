4fcf2cd0c3d84553d861c26f814e185f7f5161a4
Sharon Dagan <sharon.dagan@gmail.com>
2025-08-16 16:46:03 +0300

    Update variables.tf

--- src/variables.tf
+++ src/variables.tf
@@ -12,3 +12,9 @@ variable "transit_gateway_route_table_id" {
   type        = string
   description = "ID of the Transit Gateway Route Table"
 }
+
+variable "vpc_component_name" {
+  type        = string
+  description = "The name of the vpc component"
+  default     = "vpc"
+}
