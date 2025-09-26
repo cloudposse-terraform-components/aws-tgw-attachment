8b92d760a74c5a373ddbba81b1e4b3ca6960d1dd
Dan Miller <miller0daniel@gmail.com>
2025-09-26 14:50:45 -0400

    Add support for passing VPC and subnet IDs directly (#41)

--- src/variables.tf
+++ src/variables.tf
@@ -18,3 +18,15 @@ variable "vpc_component_name" {
   description = "The name of the vpc component"
   default     = "vpc"
 }
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC to attach to"
+  default     = null
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "IDs of the private subnets to attach to. Required if `vpc_id` is defined."
+  default     = []
+}
